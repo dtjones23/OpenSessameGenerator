1aaf54de9c127121ae34104b32650cc5cd38ac46
DeAngelo Jones <deangelojones0708@gmail.com>
2023-09-25 12:25:14 -0500

    adjusted alignment of the generateP function and corrected end tag notation

--- Develop/script.js
+++ Develop/script.js
@@ -12,18 +12,31 @@ function writePassword() {
 
   passwordText.value = password;
 
-
-  function generatePassword (){
-    var validChar = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*()[]\|"
-    // added list for all valid characters
-
-    for ( i=0; i < newPass.length; i++)
-    newPass += validChar[Math.floor(Math.random() * validChar.length)];
-  // how the code will generate characters randomly
-  }
+// this function will generate password
+function generatePassword (){
+  var passLength = 8;
+  var newPass ="";
     
+  var validChar = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*()[]\|";
+  // added list for all valid characters
+  // array of all character types
 
-} return newPass;
+  for ( let i=0; i < passLength; i++)
+  newPass += validChar[Math.floor(Math.random() * validChar.length)];
+  // how the code will generate characters randomly
+
+  // set min and max for valid characters
+  // logs string to the window
+  if (newPass.length < 8) {
+    window.prompt ("Password must be 8-128 characters and include at least 1 special character.")
+  } else if (newPass.length < 12) {
+    window.prompt ("Password must be 8-128 characters and include at least 1 special character.")
+  }
+
+};
+  return(newPass);
+};
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
