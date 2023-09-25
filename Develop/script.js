dfc36fde128652161ef0f5001a204f452656574c
DeAngelo Jones <deangelojones0708@gmail.com>
2023-09-25 15:05:46 -0500

    modified array of validChar

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,10 @@
 // Assignment code here
 var passLength = 8;
 var newPass ="";
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var validChar = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m","Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M","1","2","3","4","5","6","7","8","9","0","!","@","#","$","%","^","&","*","(",")","[","]","\","|""];
+// Array of valid characters 
 
 // Write password to the #password input
 function writePassword() {
@@ -11,15 +12,12 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
 
 // this function will generate password
 function generatePassword (){
-  var passLength = 8;
-  var newPass ="";
-    
-  var validChar = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*()[]\|";
+  var validChar = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*()[]\|"
   // added list for all valid characters
-  // array of all character types
 
   for ( let i=0; i < passLength; i++)
   newPass += validChar[Math.floor(Math.random() * validChar.length)];
@@ -33,8 +31,8 @@ function generatePassword (){
     window.prompt ("Password must be 8-128 characters and include at least 1 special character.")
   }
 
-};
-  return(newPass);
+  // This will log the new password and will ask for confirmation in the window
+  return confirm(newPass);
 };
 
 
