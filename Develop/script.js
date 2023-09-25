a69e6084385b8761512746e2b105805d493df79d
DeAngelo Jones <deangelojones0708@gmail.com>
2023-09-24 20:30:07 -0500

    added list of valid characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,6 @@
 // Assignment code here
-
+var passLength = 0;
+var newPass ="";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -13,13 +14,12 @@ function writePassword() {
 
 
   function generatePassword (){
-    var newPass = "TEST";
-
-
-    return newPass;
+    var validChar = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*()[]\|"
+    // added list for all valid characters
   }
+    
 
-}
+} return newPass;
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
