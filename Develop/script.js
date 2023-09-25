b0cbc85bcbb7b4e9bd3bd7a89a9ee0012355aaf6
DeAngelo Jones <deangelojones0708@gmail.com>
2023-09-25 15:57:56 -0500

    replaced methods to alert and moved vars to local space

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,9 @@
 // Assignment code here
-var passLength = 8;
-var newPass ="";
+// Array of valid characters
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-var validChar = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m","Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M","1","2","3","4","5","6","7","8","9","0","!","@","#","$","%","^","&","*","(",")","[","]","\","|""];
-// Array of valid characters 
+
 
 // Write password to the #password input
 function writePassword() {
@@ -16,22 +15,28 @@ function writePassword() {
 
 // this function will generate password
 function generatePassword (){
-  var validChar = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*()[]\|"
-  // added list for all valid characters
+  var passLength = 8;
+  var newPass ="";
+  var validChar = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m","Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M","1","2","3","4","5","6","7","8","9","0","!","@","#","$","%","^","&","*","(",")","[","]","\","|""]
+  // Array of valid characters 
 
-  for ( let i=0; i < passLength; i++)
+  window.alert ("You are now generating a password!")
+  // logs in pop-up window
+
+  for ( let i=0; i < passLength; i++) {
   newPass += validChar[Math.floor(Math.random() * validChar.length)];
+  }
   // how the code will generate characters randomly
 
   // set min and max for valid characters
-  // logs string to the window
+  // logs string to window and prompts the requirements
   if (newPass.length < 8) {
-    window.prompt ("Password must be 8-128 characters and include at least 1 special character.")
-  } else if (newPass.length < 12) {
-    window.prompt ("Password must be 8-128 characters and include at least 1 special character.")
+    window.alert ("Password must be 8-128 characters and include correct character types.")
+  } else if (newPass.length < 128) {
+    window.alert ("Password must be 8-128 characters and include correct character types.")
   }
 
-  // This will log the new password and will ask for confirmation in the window
+  // This will log the new password and confirm in window
   return confirm(newPass);
 };
 
